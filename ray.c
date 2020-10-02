21d3f6924bd55c82fdf2fa7a35efff9e19bf01d4
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-10-02 14:13:58 +0800

    rendering the sphere using normal vectors

--- ray.c
+++ ray.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <math.h>
 #include "ray.h"
 #include "color.h"
 
@@ -27,7 +28,7 @@ void ray_at(ray_t *ray, float t, point3_t *point_result)
 	vec3_add((vec3_t *)&ray->orig, &t_times_dir, (vec3_t *)point_result);
 }
 
-bool hit_sphere(point3_t *center, float radius, ray_t *ray)
+float hit_sphere(point3_t *center, float radius, ray_t *ray)
 {
 	vec3_t oc;
 	vec3_sub(&ray->orig, center, &oc);
@@ -35,20 +36,37 @@ bool hit_sphere(point3_t *center, float radius, ray_t *ray)
 	float a = vec3_dot_product(&ray->dir, &ray->dir);
 	float b = 2.0f * vec3_dot_product(&oc, &ray->dir);
 	float c = vec3_dot_product(&oc, &oc) - (radius * radius);
-
 	float discriminant = b*b - 4.0f*a*c;
 
-	return (discriminant > 0.0f);
+	if(discriminant < 0.0f) {
+		return -1.0f;
+	} else {
+		return (-b - sqrt(discriminant)) / (2.0f * a);
+	}
 }
 
 void ray_color(ray_t *ray, color_t *pixel_color)
 {
+	float t; //color blending ratio
+
 	/* create a sphere */
 	point3_t sphere_center;
 	point3_set(&sphere_center, 0.0f, 0.0f, -1.0f);
 
-	if(hit_sphere(&sphere_center, 0.5, ray) == true) {
-		color_set(pixel_color, 1.0f, 0.0f, 0.0f);
+	t = hit_sphere(&sphere_center, 0.5f, ray);
+	if(t > 0.0f) {
+		point3_t ray_point;
+		ray_at(ray, t, &ray_point);
+
+		vec3_t normal_vec, tmp;
+		vec3_sub(&ray_point, &sphere_center, &tmp);
+		vec3_unit_vector(&tmp, &normal_vec);
+
+		color_set(pixel_color,
+                          0.5f * vec3_get_x(&normal_vec) + 1.0f,
+                          0.5f * vec3_get_y(&normal_vec) + 1.0f,
+                          0.5f * vec3_get_z(&normal_vec) + 1.0f);
+
 		return;
 	}
 
@@ -56,7 +74,7 @@ void ray_color(ray_t *ray, color_t *pixel_color)
 	vec3_unit_vector(&ray->dir, &unit_ray_dir);
 
 	/* unit y direction range = -1 ~ 1*/
-	float t = 0.5f * (vec3_get_y(&unit_ray_dir) + 1.0f);
+	t = 0.5f * (vec3_get_y(&unit_ray_dir) + 1.0f);
 
 	color_t color1, color2;
 	color_set(&color1, 1.0f, 1.0f, 1.0f);
